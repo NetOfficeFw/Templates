e409aca3412437c50a8459b3dd547a346b20d720
Jozef Izso <jozef.izso@gmail.com>
2020-08-13 19:41:18 +0200

    Word Add-in project template

---
+++ src/WordAddinTemplate/MyAddin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using NetOffice.Tools;
+using NetOffice.WordApi;
+using NetOffice.WordApi.Tools;
+
+namespace $safeprojectname$
+{
+    [ComVisible(true)]
+    [Guid("$guid3$")]
+    [ProgId("$safeprojectname$.MyAddin")]
+    [COMAddin("MyAddin", "Addin description.", LoadBehavior.LoadAtStartup)]
+    public class MyAddin : COMAddin
+    {
+        public MyAddin()
+        {
+            this.OnConnection += MyAddin_OnConnection;
+        }
+
+        private void MyAddin_OnConnection(object application, ext_ConnectMode connectMode, object addInInst, ref Array custom)
+        {
+            this.Application.DocumentOpenEvent += Application_DocumentOpenEvent;
+        }
+
+        private void Application_DocumentOpenEvent(Document doc)
+        {
+            using (doc)
+            {
+                // start working with the document
+            }
+        }
+    }
+}

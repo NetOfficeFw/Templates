315621f9eab608ed76ab87623dfef3a2ea03d015
Jozef Izso <jozef.izso@gmail.com>
2020-08-16 18:25:35 +0200

    Add PowerPoint Add-in project template

---
+++ src/PowerPointAddinTemplate/MyAddin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using NetOffice.Tools;
+using NetOffice.PowerPointApi;
+using NetOffice.PowerPointApi.Tools;
+
+namespace $safeprojectname$
+{
+    [ComVisible(true)]
+    [Guid("$guid3$")]
+    [ProgId("$safeprojectname$.MyAddin")]
+    [COMAddin("MyAddin", "Addin description.", LoadBehavior.LoadAtStartup)]
+    public class MyAddin : COMAddin
+    {
+        public MyAddin()
+        {
+            this.OnConnection += MyAddin_OnConnection;
+        }
+
+        private void MyAddin_OnConnection(object application, ext_ConnectMode connectMode, object addInInst, ref Array custom)
+        {
+            this.Application.PresentationOpenEvent += Application_PresentationOpenEvent;
+        }
+
+        private void Application_PresentationOpenEvent(Presentation presentation)
+        {
+            using (presentation)
+            {
+                // start working with the workbook
+            }
+        }
+    }
+}
